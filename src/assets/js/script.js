dae3e90d12a14b9840fd10e7709d8da8fc067a99
Hugo C <lumap@duck.com>
2023-11-13 10:07:33 +0100

    fix

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -4,6 +4,18 @@ $(document).ready(function () {
         $("#modal-mobilemayhaveissues").hide();
     });
 
+    $(".modal-backdrop").on("click", () => {
+        $(".modal-root").fadeOut();
+    });
+
+    $(".job.see-more").on("click", () => {
+        $(".jobs-details").fadeIn();
+    });
+
+    $(".formation.see-more").on("click", () => {
+        $(".formations-details").fadeIn();
+    });
+
     new QRCode(
         "qrcode", {
         text: document.location.href,
@@ -16,6 +28,10 @@ $(document).ready(function () {
     );
 
     $("#savetopdf").on("click", () => {
+        $("#card-footer").hide();
+        $("#card-qr").css("display", "flex");
+        $(".see-more").css("display", "none");
+        $(".see-more-online").css("display", "flex");
         const quality = 2.5; // Higher the better but larger file
         html2canvas(document.querySelector('#cv'),
             { scale: quality }
@@ -23,6 +39,10 @@ $(document).ready(function () {
             const pdf = new jsPDF('p', 'mm', 'a4');
             pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
             pdf.save("cv_hugo_chretien.pdf");
+            $("#card-footer").show();
+            $("#card-qr").hide();
+            $(".see-more").css("display", "flex");
+            $(".see-more-online").css("display", "none");
         });
     });
 });
